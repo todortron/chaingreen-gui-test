36757f9ca09aa5454b4494e11918a54637249990
Todor <todor@gostartups.net>
2021-10-25 13:52:16 +0300

    Return to Chia icon.

--- src/components/icons/Chaingreen.tsx
+++ src/components/icons/Chaingreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SvgIcon, SvgIconProps } from '@material-ui/core';
-import { ReactComponent as ChaingreenIcon } from './images/chaingreen.svg';
+import { ReactComponent as ChaingreenIcon } from './images/chia.svg';
 
 export default function Keys(props: SvgIconProps) {
   return <SvgIcon component={ChaingreenIcon} viewBox="0 0 800 600" {...props} />;
